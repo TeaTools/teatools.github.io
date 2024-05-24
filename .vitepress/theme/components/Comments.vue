192d3e52be5490d6c824a872cc997feea3fa2062
nanfang <nanfangzhe@qq.com>
2024-05-24 12:41:02 +0800

    fix【Gitalk】

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -14,8 +14,8 @@ const gitalk = new Gitalk({
     clientID: "Ov23licokG6VwYrzJEb3", // 修改 clinetID（参考：https://zhuanlan.zhihu.com/p/260781932）
     clientSecret: "80cbd3ead32aac4722bb7dc299079e6bd9ef6958",
     repo: "teatools.github.io",
-    owner: "tea-blog",
-    admin: ["tea-blog"],
+    owner: "teatools",
+    admin: ["teatools"],
     id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
     language: "zh-CN",
     distractionFreeMode: true, // Facebook-like distraction free mode
